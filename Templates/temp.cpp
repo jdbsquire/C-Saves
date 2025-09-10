d6cca4fd0e9cabcf3faf65fecfb065358b3c5303
James Boyce <61768977+jdbsquire@users.noreply.github.com>
2025-09-10 15:09:36 +0000

    Errors between .h and .cpp file need to make the names consistent

--- Templates/temp.cpp
+++ Templates/temp.cpp
@@ -6,5 +6,23 @@
 using namespace std; //no need to place std::before the variables
 
 //Implement Constructors and methods
-temp::temp(){} //constructor to hold args and member variables
+temp::temp() : 
+    fName(""),
+    lName(""),
+    birthYear(0),
+    presentYear(0) {} //constructor to hold args and member variables
 
+temp::temp(string newFName, string newLName) : 
+    string fName(newFName),
+    string lNames(newLName),
+    int birthYear(0),
+    int presentYear(0) {}
+
+temp::temp(string newFName, string newLName, int newBirthYear, int newPresentYear)
+
+
+//Setters
+
+//Getters
+
+//Methods
