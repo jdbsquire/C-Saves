b341f9d168f34190e5135c2126818c381a600190
James Boyce <61768977+jdbsquire@users.noreply.github.com>
2025-09-10 14:57:25 +0000

    Initial Commit of Templates

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # C-Saves
 Repo for the exercises in the book. It may hold some original templates.
+I intend this to be a folder to contain other projects from the book
+C++ How to Program 10th Edition
---
+++ Templates/temp.cpp
@@ -0,0 +1,10 @@
+#include "temp.h"
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+using namespace std; //no need to place std::before the variables
+
+//Implement Constructors and methods
+temp::temp(){} //constructor to hold args and member variables
+
