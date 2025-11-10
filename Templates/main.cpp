29284436d67b9f1cb41faa5d12d7bf5bd98b74aa
James Boyce <61768977+jdbsquire@users.noreply.github.com>
2025-11-10 03:40:18 +0000

    Created a main.cpp template

---
+++ Templates/main.cpp
@@ -0,0 +1,20 @@
+
+
+#include <algorithm>
+#include <array>
+#include <cctype>
+#include <cmath>
+#include <cstddef>
+#include <cstdlib>
+#include <ctime>
+#include <exception>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <random>
+#include <string>
+#include <vector>
+
+int main(){
+    return 0;
+}
