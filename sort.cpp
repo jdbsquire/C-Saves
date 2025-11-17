eee2305984acf8d8d3db7e147e7f825f9aa05221
James Boyce <61768977+jdbsquire@users.noreply.github.com>
2025-11-17 20:30:15 +0000

    Testing a generic sorting algorithm

---
+++ sort.cpp
@@ -0,0 +1,40 @@
+#include <algorithm>
+#include <array>
+#include <cctype>
+#include <cmath>
+#include <cstddef>
+#include <cstdlib>
+#include <ctime>
+#include <exception>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <random>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main(){
+
+    //assign a name to a filename for use in the sorting program
+    string fileName;
+    fileName = "PersonSorted.txt";
+    //open intended file
+    ifstream infile(fileName);
+    //see if the file opened
+    if (!infile) {
+        cerr << "Error opening " << fileName << endl;
+        return 1;
+    } else {
+        cout  << "File Opened!\n";
+    }
+
+    //create vector to receive the file
+    //vector<SortedFile> people; 
+
+    //close the file always
+    infile.close();
+
+    return 0;
+}
